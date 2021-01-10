105d28d5eeeac15f276d7604c2c72a8d8efc567c
Valentin Moreau <moreauv.04@gmail.com>
2021-01-10 12:00:25 +0100

    Setup gestion des arguments

--- escalier.js
+++ escalier.js
@@ -1,1 +1,13 @@
-console.log("Hello World !");
+#!/usr/local/bin/node
+
+let nb;
+// parcours les arguments
+process.argv.forEach(function (val, index) {
+    if (index === 2) { //recupere le premier argument
+        nb = val;
+    } else {
+        nb = 0;
+    }
+});
+
+console.log(nb);
