965cb8b6c7c34b04bf759b2387f24ea2e68fc46a
Valentin Moreau <moreauv.04@gmail.com>
2021-01-11 09:40:44 +0100

    Escalier fonctionnel

--- escalier.js
+++ escalier.js
@@ -1,6 +1,7 @@
 #!/usr/local/bin/node
 
 let nb;
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 // parcours les arguments
 process.argv.forEach(function (val, index) {
     if (index === 2) { //recupere le premier argument
@@ -10,4 +11,15 @@ process.argv.forEach(function (val, index) {
     }
 });
 
-console.log(nb);
+output = [];
+
+for (i = 0; i < nb; i++) {
+    output.push(" "); //rempli le tableau d'espace vide
+}
+
+for (i = nb-1; i >= 0; i--) {
+    output[i] = "#";
+    console.log(output.reduce(reducer));
+}
+
+
