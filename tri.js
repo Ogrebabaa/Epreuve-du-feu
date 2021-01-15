20ff95aaf071d794410a9ca13814c0d5438640f8
Valentin Moreau <moreauv.04@gmail.com>
2021-01-15 11:39:52 +0100

    Tri fonctionnel + setup carre.js

--- tri.js
+++ tri.js
@@ -1,16 +1,52 @@
 #!/usr/local/bin/node
 tab_arg = [];
+tab_res = [];
+
 // parcours les arguments
 process.argv.forEach(function (val, index) {
     if (index >= 2) { //recupere le premier argument
         tab_arg.push(val);
     } 
+
 });
 
+let getMinus = (arr) => {
+    let minus = arr[0];
+    arr.forEach(element => {
+        if (element < minus) {
+            minus = element;
+        }
+    });
+
+    return minus;
+}
+
+
+let afficheArr = (arr) => {
+    let res = "";
+    arr.forEach(element => {
+        res += element + " ";
+    })
+    console.log(res);
+}
+
 //algorithme de tri
-if (tab_arg != null) {
-    console.log(tab_arg);
+if (tab_arg.length != 0) {
+
+    let iterations = tab_arg.length;
+
+    for (i = 0; i < iterations; i++) {
+        
+        let valueToInsert = getMinus(tab_arg);;
+        let index_value = tab_arg.indexOf(valueToInsert);
+        tab_res.push(valueToInsert);
+        tab_arg.splice(index_value, 1);
+        
+    }
+
+    //affichage
+    afficheArr(tab_res);
 } else {
-    console.log("J'ai besoin d'un paramêtre !");
+    console.log("J'ai besoin de paramètre !");
 }
 
